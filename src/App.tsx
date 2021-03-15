b0a4fb86e84ff42276689ed952221fc581f41520
bones-bones <elliot.ma.brown@gmail.com>
2021-03-14 23:06:58 -0700

    it lives

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { addToStepHook } from './stepCounter';
+
+import styled from '@emotion/styled';
+import useEvent from './stepCounter/stepCounter';
 
 function App() {
+  const [stepValue, setStepValue] = useState<number>(0)
+
+  const incrementStepValue = () => {
+    setStepValue(stepValue + 1);
+  }
+  useEvent(incrementStepValue)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <StepValue stepVal={stepValue}>{stepValue}</StepValue>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
 }
+
+const StepValue = styled.div<{ stepVal: number }>`
+height:50vh;
+width:50vw;
+background-color: ${({ stepVal }) => {
+    if (stepVal > 0.3) {
+      return 'GREEN';
+    }
+    if (stepVal < -0.3) { return 'RED' }
+    return 'GREY';
+  }};
+`
+
 
 export default App;
---
+++ src/stepCounter/stepCounter.ts
@@ -0,0 +1,60 @@
+
+const callbacks: ((args: any) => void)[] = [];
+const sensor = new LinearAccelerationSensor();
+enum Direction {
+    POS = 'POS',
+    NEG = 'NEG'
+}
+
+
+let lastVersion = Direction.NEG;
+let z = 0;
+
+
+function handleHandleStep(handler: (args?: any) => any) {
+
+    return (event: Event) => {
+        if (sensor.z !== z) {
+            z = (sensor.z || 0);
+            // This is where i assume i care because it is a big deal
+            if (Math.abs(z) > 0.3) {
+
+                const currentVersion = z > 0 ? Direction.POS : Direction.NEG;
+
+                if (currentVersion != lastVersion) {
+                    handler();
+                    lastVersion = currentVersion;
+                }
+            }
+        }
+    }
+}
+
+
+export const addToStepHook = (func: (args: any) => any) => {
+    callbacks.push(func);
+}
+
+
+
+
+
+
+import { useEffect } from "react"
+
+export default function useEvent(handler: (args: any) => any, passive = false) {
+    let metaHandler = handleHandleStep(handler)
+    useEffect(() => {
+        // initiate the event handler
+        sensor.addEventListener('reading', metaHandler, passive)
+        sensor.start()
+
+
+        // this will clean up the event every time the component is re-rendered
+        return function cleanup() {
+            sensor.removeEventListener('reading', metaHandler)
+            sensor.stop()
+
+        }
+    })
+}
